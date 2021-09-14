f83198df80d9f5d6626671d6abd6ec1bd178992e
Christian <chrmon2@nilesk12.org>
2021-09-13 20:55:19 -0500

    3000

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.use(function(req,res){
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);

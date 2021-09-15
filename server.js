ab15d972cca71e883ddf773c598934ab6d07f39b
Christian <chrmon2@nilesk12.org>
2021-09-15 14:35:48 -0500

    port

--- server.js
+++ server.js
@@ -84,7 +84,9 @@ app.use(function(req,res){
     })
 });
 
-app.listen(process.env.PORT || 3000, function(){
+const port = process.env.PORT || 3000
+
+app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
